075a0105a47aa3f07d6221ac3103bc566457bc80
iStubsy <63608655+iStubsy@users.noreply.github.com>
2023-05-15 17:35:15 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 window.onscroll = function () {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        document.querySelector(".navigationBar").style.opacity = 0.1;
+
     } else {
-        document.querySelector(".navigationBar").style.opacity = 1;
+
     }
 
     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
         document.querySelector(".navigationBar").classList.add("navigationBarScrolled");
-        document.querySelector(".navigationBar").style.opacity = 1;
 
     } else {
         document.querySelector(".navigationBar").classList.remove("navigationBarScrolled");
@@ -15,7 +14,7 @@ window.onscroll = function () {
 }
 
 document.querySelector("#hamburgerMenu").onclick = function () {
-    //document.querySelector(".overlayHide").classList.toggle("overlayShow");
-    //document.getElementsByTagName("HTML")[0].style.overflow = "hidden";
-    //document.querySelector("#closeButton").style.display = "flex";
+    document.querySelector(".overlayHide").classList.toggle("overlayShow");
+    document.getElementsByTagName("HTML")[0].style.overflow = "hidden";
+    document.querySelector("#closeButton").style.display = "flex";
 }
