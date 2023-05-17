de81320939525d60a670c5051e4502a7955a791c
iStubsy <63608655+iStubsy@users.noreply.github.com>
2023-05-17 15:40:11 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -16,9 +16,16 @@ window.onscroll = function () {
 document.querySelector("#hamburgerMenu").onclick = function () {
     document.querySelector(".overlay").classList.toggle("overlayShow");
     document.getElementsByTagName("HTML")[0].style.overflow = "hidden";
+
+    //document.querySelector(".hmlTransform1").style.transform = "rotate(45deg)";
+    //document.querySelector(".hmlTransform2").style.transform = "rotate(-45deg)";
 }
 
 document.querySelector(".closeButton").onclick = function () {
     document.querySelector(".overlay").classList.toggle("overlayShow");
     document.getElementsByTagName("HTML")[0].style.overflow = "auto";
+
+    for (let i = 0; i < document.querySelectorAll(".hamburgerMenuLine").length; i++) {
+        document.querySelectorAll(".hamburgerMenuLine")[i].style.margin = "10px";
+    }
 }
