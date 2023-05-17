2d5ac77ae34ec23c1b60d47c0af0dca9fe8ccd9a
iStubsy <63608655+iStubsy@users.noreply.github.com>
2023-05-17 19:31:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ window.onscroll = function () {
 
     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
         document.querySelector(".navigationBar").classList.add("navigationBarScrolled");
+        document.querySelector("#hamburgerMenu").style.width = "65px";
 
     } else {
         document.querySelector(".navigationBar").classList.remove("navigationBarScrolled");
+        document.querySelector("#hamburgerMenu").style.width = "80px";
     }
 }
 
@@ -17,16 +19,23 @@ document.querySelector("#hamburgerMenu").onclick = function () {
     document.querySelector(".overlay").classList.toggle("overlayShow");
     document.getElementsByTagName("HTML")[0].style.overflow = "hidden";
 
-    //document.querySelector(".hmlTransform1").style.transform = "rotate(45deg)";
-    //document.querySelector(".hmlTransform2").style.transform = "rotate(-45deg)";
+    document.querySelector(".hmlTransform1").style.transform = "rotate(45deg)";
+    document.querySelector(".hmlTransform2").style.transform = "rotate(-45deg)";
+
+    for (let i = 0; i < document.querySelectorAll(".hamburgerMenuLine").length; i++) {
+        document.querySelectorAll(".hamburgerMenuLine")[i].style.margin = "8px auto";
+    }
 }
 
 document.querySelector(".closeButton").onclick = function () {
+    document.querySelector(".hmlTransform1").style.transform = "rotate(0)";
+    document.querySelector(".hmlTransform2").style.transform = "rotate(0)";
+
     document.querySelector(".overlay").classList.toggle("overlayShow");
     document.getElementsByTagName("HTML")[0].style.overflow = "auto";
 
     for (let i = 0; i < document.querySelectorAll(".hamburgerMenuLine").length; i++) {
-        document.querySelectorAll(".hamburgerMenuLine")[i].style.margin = "10px";
+        document.querySelectorAll(".hamburgerMenuLine")[i].style.margin = "4px auto";
     }
 }
 
