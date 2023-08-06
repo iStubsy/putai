24d43710c9c87879ce117c8ca22008ac662438c4
iStubsy <63608655+iStubsy@users.noreply.github.com>
2023-08-06 17:59:56 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ window.onscroll = function () {
     } else {
         document.querySelector(".navigationBar").classList.remove("navigationBarScrolled");
         document.querySelector(".navigationBarInner").classList.remove("navigationBarInnerScrolled");
-        
+
         document.querySelector(".hamburgerMenu").classList.remove("hamburgerMenuScrolled");
         document.querySelectorAll(".hamburgerMenuLine")[0].classList.remove("hamburgerMenuLineScrolled");
         document.querySelectorAll(".hamburgerMenuLine")[1].classList.remove("hamburgerMenuLineScrolled");
@@ -48,7 +48,7 @@ document.querySelector(".backToTop").onclick = function () {
     document.documentElement.scrollTop = 0;
 }
 
-
+/*
 $(document).ready(function () {
     $(document).scroll(function (evt) {
         var v2 = Math.abs($('.cardImage-1').position().top - $(window).height() / 2 + 100);
@@ -61,3 +61,23 @@ $(document).ready(function () {
         }
     });
 });
+*/
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
