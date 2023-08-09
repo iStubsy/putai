bbbd64f2dd1f6ca6d206272c0b5b7b90167ef6e3
iStubsy <63608655+iStubsy@users.noreply.github.com>
2023-08-09 21:19:05 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -68,11 +68,11 @@ function reveal() {
     for (var i = 0; i < reveals.length; i++) {
         var windowHeight = window.innerHeight;
         var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
+        var elementVisible = 100;
         if (elementTop < windowHeight - elementVisible) {
             reveals[i].classList.add("active");
         } else {
-            reveals[i].classList.remove("active");
+            //reveals[i].classList.remove("active");
         }
     }
 }
