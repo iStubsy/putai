f3dde3ae38c583728760da40aa961dd874c4d89c
iStubsy <63608655+iStubsy@users.noreply.github.com>
2023-09-30 17:33:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -194,6 +194,20 @@
      }
  }
 
+function opacityReveal() {
+     var reveals = document.querySelectorAll(".opacityReveal");
+     for (var i = 0; i < reveals.length; i++) {
+         var windowHeight = window.innerHeight;
+         var elementTop = reveals[i].getBoundingClientRect().top;
+         var elementVisible = 150;
+         if (elementTop < windowHeight - elementVisible) {
+             reveals[i].classList.add("active");
+         } else {
+             reveals[i].classList.remove("active");
+         }
+     }
+ }
+
 
 
 
@@ -213,7 +227,9 @@
  window.addEventListener("scroll", reveal);
  window.addEventListener("scroll", revealo);
  window.addEventListener("scroll", moveUpRevealFun);
+ window.addEventListener("scroll", opacityReveal);
 
  // To check the scroll position on page load
  reveal();
  moveUpRevealFun();
+opacityReveal();
