1dff146dfa2851708f121fc7661a192d3f4e9e52
iStubsy <63608655+iStubsy@users.noreply.github.com>
2023-12-13 20:49:39 +0100

    Add files via upload

--- heightScript.js
+++ heightScript.js
@@ -1,9 +1,5 @@
-alert("hello2");
-
-const appHeight = () => {
-    const doc = document.documentElement
-    doc.style.setProperty('--app-height', `${window.innerHeight}px`);
-}
-
-window.addEventListener('resize', appHeight);
-appHeight();
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+// We listen to the resize event
