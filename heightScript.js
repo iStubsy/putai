c296e777de59e4081d357c4089d6fa44c116a9f8
iStubsy <63608655+iStubsy@users.noreply.github.com>
2023-12-27 11:03:33 +0100

    Add files via upload

--- heightScript.js
+++ heightScript.js
@@ -4,9 +4,10 @@ let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 // We listen to the resize event
 
-
+/*
 window.addEventListener('resize', () => {
     // We execute the same script as before
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
+*/
